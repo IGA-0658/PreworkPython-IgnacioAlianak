93f45d46eabf1411b7204863b447517045f44afc
Ignacio Alianak <ignacioalianak@gmail.com>
2024-01-29 18:24:44 +0100

    avance ejercicios

--- ejercicios.py
+++ ejercicios.py
@@ -1,17 +1,75 @@
-#Variables y Operadores
-"""1/
+#//////////// Variables y Operadores ////////////
+"""1. Ejercicio: Crea una variable llamada nombre y asígnale tu nombre como valor. Luego, imprime la variable.
 nombre = "ignacio"
 print(nombre)
 """
-"""2/
+"""2.  Ejercicio: Crea dos variables, a y b , y asígnales los valores 5 y 10
 a = 5
 b = 10
 suma = a + b
 print(suma)
 """
-"""3/
-print(10*5)
+"""3. Ejercicio: Calcula el área de un triángulo con base 10 y altura 5.
+altura = 5
+base = 10
+area = (base * altura) / 2
+print(area)
 """
-"""4/
+"""4. Ejercicio: Calcula el resto de dividir 17 entre 3.
 print(17/3)
+"""
+# //////////// Condicionales ////////////
+"""1. Ejercicio: Dado un número, imprime si es positivo o negativo.
+numero = -10
+if numero > 0:
+  print("positivo")
+else:
+  print("negativo")  
+"""
+"""2. Ejercicio: Dado un número, imprime si es par o impar.
+numero = 8
+if numero % 2 == 0:
+  print("par")
+else:
+  print("impar")  
+"""
+"""3. Ejercicio: Dado tres números, encuentra y muestra el mayor de ellos.
+a, b, c = 5, 25, 15
+mayor = max(a, b, c)
+print(f"El mayor de los tres números es: {mayor}")
+"""
+#//////////// Bucles ////////////
+"""1. Ejercicio: Imprime los números del 1 al 10 usando un bucle for .
+for i in range(1,11):
+  print(i)
+"""
+"""2. Ejercicio: Imprime los números pares del 1 al 20 usando un bucle while .
+i=1
+while i <=20:
+  if i % 2 == 0:
+    print(i)
+  i+=1
+"""
+"""
+3. Ejercicio: Usa un bucle para calcular la suma de los números del 1 al 100.
+suma = 0
+for numero in range(1,101):
+  suma += numero
+print("La suma es:", suma)
+"""
+#//////////// Funciones ////////////
+"""1. Ejercicio: Define una función que tome dos números y retorne su suma.
+"""
+"""
+2. Ejercicio: Defineuna función que tome un número y retorne su factorial.
+"""
+"""
+3. Ejercicio: Define una función que tome un número y determine si es primo.
+"""
+"""
+4. Ejercicio: Define una función que reciba una lista de números y retorne la suma de ellos.
+"""
+"""
+5. Ejercicio: Define una función que reciba una cadena de texto y retorne la
+cadena en reversa.
 """
